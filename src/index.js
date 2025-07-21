d937115cc03a4655c0fd7f80bda48f4019492eb6
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-07-21 14:33:30 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const arr = Math.abs(n).toString().split('');
+  return Number(arr.reverse().join(''));
 };
